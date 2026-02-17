dd92e8a1853b282ca6529f0826cf5fc582844437
as3405-collab <as3405@srmist.edu.in>
2026-02-17 15:19:49 +0530

    Added UC2

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,11 +1,5 @@
-public class OopsBannerApp {
-    public static void main(String[] args) {
-        System.out.println(" ***   ***   *****   ***** ");
-        System.out.println("** ** ** **  ** **  **   ");
-        System.out.println("** ** ** **  ** **  **   ");
-        System.out.println("** ** ** **  *****   ***** ");
-        System.out.println("** ** ** **  **         **");
-        System.out.println("** ** ** **  **         **");
-        System.out.println(" ***   ***   **      ***** ");
-    }
+public class OopsBannerApp{
+public static void main(String [] args){
+System.out.println("OOPS");
+}
 }
