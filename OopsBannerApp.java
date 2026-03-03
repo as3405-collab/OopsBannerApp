69fe25279b1d9e506a5a42a7b80a35e611bbe761
as3405-collab <as3405@srmist.edu.in>
2026-03-03 12:58:08 +0530

    Added UC5

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,20 +1,15 @@
 public class OopsBannerApp {
-
     public static void main(String[] args) {
-
-        // Declare and initialize array in a single statement using String.join()
-        String[] banner = {
-                String.join("", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*"),
-                String.join("", "*", " ", "W", "E", "L", "C", "O", "M", "E", " ", "*"),
-                String.join("", "*", " ", "T", "O", " ", "O", "O", "P", "S", " ", "*"),
-                String.join("", "*", " ", "B", "A", "N", "N", "E", "R", " ", "*"),
-                String.join("", "*", " ", "A", "P", "P", " ", "U", "C", "5", " ", "*"),
-                String.join("", "*", " ", "J", "A", "V", "A", " ", "D", "E", "M", "O"),
-                String.join("", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*")
+		String[] lines = {
+            String.join(" ", " ***  ", " ***  ", "****  ", " **** "),
+            String.join(" ", "*   * ", "*   * ", "*   * ", "*     "),
+            String.join(" ", "*   * ", "*   * ", "*   * ", "*     "),
+            String.join(" ", "*   * ", "*   * ", "****  ", " ***  "),
+            String.join(" ", "*   * ", "*   * ", "*     ", "    * "),
+            String.join(" ", "*   * ", "*   * ", "*     ", "*   * "),
+            String.join(" ", " ***  ", " ***  ", "*     ", " ***  ")
         };
-
-        // Enhanced for loop to print banner
-        for (String line : banner) {
+        for (String line : lines) {
             System.out.println(line);
         }
     }
